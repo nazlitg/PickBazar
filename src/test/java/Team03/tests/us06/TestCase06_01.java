8839af888458e1cc56081b1fd650cf8a095c3ac7
uguravan <114769074+uguravan@users.noreply.github.com>
2023-01-18 02:43:22 +0300

    log

--- src/test/java/Team03/tests/us06/TestCase06_01.java
+++ src/test/java/Team03/tests/us06/TestCase06_01.java
@@ -1,11 +1,16 @@
 package Team03.tests.us06;
 
+import Team03.pages.PickBazarHomePage;
 import Team03.utilities.ConfigReader;
 import Team03.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class TestCase06_01 {
+    @Test
+    public void login(){
+    }
 }
