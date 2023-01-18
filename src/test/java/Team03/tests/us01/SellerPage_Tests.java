c9623866b9c3a5ecc880c3e9cac286d11c26c412
Teykinay <teykinaycerkezoglu@gmail.com>
2023-01-18 22:27:31 +0300

    last update

--- src/test/java/Team03/tests/us01/SellerPage_Tests.java
+++ src/test/java/Team03/tests/us01/SellerPage_Tests.java
@@ -1,25 +1,18 @@
 package Team03.tests.us01;
 
 import Team03.pages.SellerPage;
-import Team03.utilities.ConfigReader;
+
 import Team03.utilities.Driver;
 import Team03.utilities.ReusableMethods;
 import com.github.javafaker.Faker;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import javax.swing.*;
-import java.time.Duration;
-import java.util.Iterator;
-import java.util.List;
+
+import java.nio.file.Paths;
+
 
 public class SellerPage_Tests {
 
@@ -38,11 +31,11 @@ public class SellerPage_Tests {
 
         //3-Kullanıcı name , e-mail, password belirler.
         String e_mail = faker.name().firstName();
-        ReusableMethods.dataSend(sp.seller_name,"123");
+        ReusableMethods.dataSend(sp.seller_name, "123");
         sp.seller_email.click();
-        ReusableMethods.dataSend(sp.seller_email,e_mail + "@gmail.com");
+        ReusableMethods.dataSend(sp.seller_email, e_mail + "@gmail.com");
         sp.seller_password.click();
-        ReusableMethods.dataSend(sp.seller_password,"123");
+        ReusableMethods.dataSend(sp.seller_password, "123");
 
         //4-Kullanıcı şifreyi gizli ya da açık görebilir.
         sp.password_show.click();
@@ -54,30 +47,16 @@ public class SellerPage_Tests {
 
         Driver.closeDriver();
     }
+
     @Test
     public void task2() throws InterruptedException {
         SellerPage sp = new SellerPage();
         //1-Kulanıcı URL gider.
         sp.goURL();
-
         //2-Kullanıcı Become a Seller buttonuna tıklar.
-        sp.Become_Seller.click();
-        Driver.getDriver().switchTo().window((String) Driver.getDriver().getWindowHandles().toArray()[1]);
-
         //3-Kullanıcı Login linkine gider
-        sp.Login_Seller. click();
-        Thread.sleep(100);
-
-        //4-Kullanıcı e-mail ve password bilgilerini girer.
-        sp.seller_email.click();
-        Thread.sleep(500);
-        sp.seller_email.clear();
-        ReusableMethods.dataSend(sp.seller_email,"testngprojectteam03@gmail.com");
-        sp.seller_password.click();
-        sp.seller_password.clear();
-        Thread.sleep(1000);
-        ReusableMethods.dataSend(sp.seller_password,"Projeteam03-2022" +Keys.ENTER);
-
+        //4-Kullanıcı e-mail ve password bilgilerini girer
+        sp.Login();
         //5-Kullanıcı Login olduğunu doğrular.
         sp.check_enable.click();
         ReusableMethods.gorunuyorMU(sp.check_enable);
@@ -86,9 +65,60 @@ public class SellerPage_Tests {
     }
 
     @Test
-    public void task3(){
+    public void task3() throws InterruptedException {
+
+        Faker faker = new Faker();
+        Actions actions = new Actions(Driver.getDriver());
         SellerPage sp = new SellerPage();
 
+        //1-Kullanıcı URL gider.
+        sp.goURL();
+
+        //2-Kullanıcı seller olarak giriş yapar.
+        sp.Login();
+
+        //3-Kullanıcı create shop ile mağaza oluşturma adımına başlayabilmeli.(Sağ üstte).
+        sp.create_shop.click();
+
+        //4-Kullanıcı istenilen belgeleri yüklemeli bilgileri girmeli ve kaydetmeli.
+        String filepath = "C:\\Users\\tekin\\IdeaProjects\\ProjectTeam03\\src\\test\\java\\Team03\\resources\\Avatar-Free-PNG-Image.png";
+        sp.set_img.forEach(s -> s.sendKeys(filepath));
+        sp.set_information.forEach(s -> actions.moveToElement(s).click().sendKeys("123").perform());
+        actions.moveToElement(sp.add_holder_email).click().sendKeys(faker.name().firstName() + "@gmail.com");
+        sp.Add_Social_Profil.click();
+        sp.Save_Profil.click();
+
+        //5-Kullanıcı dashboard'tan My shops 'a girebilmeli ve oluşturduğu mağazayı görmeli
+        Assert.assertTrue(sp.show_shops.getText().contains("123"));
+
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void task4() throws InterruptedException {
+
+        Faker faker = new Faker();
+        Actions actions = new Actions(Driver.getDriver());
+        SellerPage sp = new SellerPage();
+
+        //1-Kullanıcı URL gider.
+        sp.goURL();
+
+        //2-Kullanıcı seller olarak giriş yapar.
+        sp.Login();
+
+        //3-Kullanıcı product sekmesini açar(sağda dashboardda).
+        sp.show_shops.click();
+        sp.WaitUntil(sp.dashboard);
+        sp.dashboard.click();
+        sp.WaitUntil(sp.products);
+        sp.products.click();
+
+
+        //4-Kullanıcı 'Doğal Kozalak Reçeli ' keyword'ünü arar ve istenilen sonucu doğrular.
+        sp.WaitUntil(sp.products_search);
+        sp.products_search.click();
+        sp.products_search.sendKeys("Doğal Kozalak Reçeli" + Keys.ENTER);
 
     }
 
