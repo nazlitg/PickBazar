5bda7c7a8036ff86e5eef5a91cb090732ff32aba
uguravan <114769074+uguravan@users.noreply.github.com>
2023-01-18 19:06:12 +0300

    us08 bitti

--- src/test/java/Team03/pages/PickBazarHomePage.java
+++ src/test/java/Team03/pages/PickBazarHomePage.java
@@ -30,4 +30,45 @@ public class PickBazarHomePage {
     @FindBy(xpath = "//p[.='The credentials was wrong!']")
     public WebElement invalidLogin;
 
+    @FindBy(xpath = "(//div[.='Grocery'])[1]//div[contains(@class,'relative')]")
+    public WebElement categoryDropDown;
+
+    @FindBy(xpath = "//button[.='Daily Needs']")
+    public WebElement newCategoryDropDown;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Grocery')]")
+    public WebElement groceryCategory;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Bakery')]")
+    public WebElement bakeryCategory;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Makeup')]")
+    public WebElement makeUpCategory;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Bags')]")
+    public WebElement bagsCategory;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Clothing')]")
+    public WebElement clothingCategory;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Furniture')]")
+    public WebElement furnitureCategory;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Daily')]")
+    public WebElement dailyNeedsCategory;
+
+    @FindBy(xpath = "//div[contains(@id,'headlessui-menu')]//span[contains(text(),'Books')]")
+    public WebElement booksCategory;
+
+    @FindBy(xpath = "//h1[@class='text-2xl font-bold tracking-tight text-heading lg:text-4xl xl:text-5xl']")
+    public WebElement categoryTitle;
+
+    @FindBy(xpath = "//h1[@class='text-2xl font-bold tracking-tight text-heading md:text-3xl lg:text-4xl xl:text-5xl !text-accent']")
+    public WebElement nextCategoryTitle;
+
+    @FindBy(xpath = "//span[contains(text(),'Chair')]")
+    public WebElement chairButton;
+
+    @FindBy(xpath = "//h3[contains(text(),'Popular')]")
+    public WebElement booksPopular;
 }
--- src/test/java/Team03/tests/us08/TestCase08_01.java
+++ src/test/java/Team03/tests/us08/TestCase08_01.java
@@ -1,5 +1,56 @@
 package Team03.tests.us08;
 
-public class TestCase08_01 {
+import Team03.pages.PickBazarHomePage;
+import Team03.utilities.ConfigReader;
+import Team03.utilities.Driver;
+import Team03.utilities.TestBaseReports;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class TestCase08_01 extends TestBaseReports {
+//1-Kullanıcı URL gider.
+//2-Kullanıcı Grocery kategorisini seçince, grocery ürünlerini görmelidir.
+//3-Kullanıcı Bakery kategorisini seçince, bakery ürünlerini görmelidir.
+//4-Kullanıcı Make up kategorisini seçince, make up ürünlerini görmelidir.
+//5-Kullanıcı Bags kategorisini seçince, bags ürünlerini görmelidir.
+//6-Kullanıcı Clothing kategorisini seçince, clothing ürünlerini görmelidir.
+//7-Kullanıcı Furniture kategorisini seçince, furniture ürünlerini görmelidir.
+//8-Kullanıcı Daily Needs kategorisini seçince, daily needs ürünlerini görmelidir.
+//9-Kullanıcı Books kategorisini seçince, books ürünlerini görmelidir.
+
+    @Test
+    public void categoryTest() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        Actions actions = new Actions(Driver.getDriver());
+        PickBazarHomePage hp = new PickBazarHomePage();
+
+        actions.moveToElement(hp.categoryDropDown).click().perform();
+        actions.moveToElement(hp.groceryCategory).click().perform();
+        Assert.assertTrue(hp.categoryTitle.getText().contains("Groceries"), "Grocery category is not displayed.");
+
+        actions.moveToElement(hp.bakeryCategory).click().perform();
+        Thread.sleep(2000);
+        Assert.assertTrue(hp.categoryTitle.getText().toLowerCase().contains("bakery"), "Bakery category is not displayed.");
+
+        actions.moveToElement(hp.makeUpCategory).click().perform();
+        Assert.assertTrue(hp.categoryTitle.getText().toLowerCase().contains("makeup"), "Makeup category is not displayed.");
+
+        actions.moveToElement(hp.bagsCategory).click().perform();
+        Assert.assertTrue(hp.categoryTitle.getText().toLowerCase().contains("bags"), "Bags category is not displayed.");
+
+        actions.moveToElement(hp.clothingCategory).click().perform();
+        Assert.assertTrue(hp.categoryTitle.getText().toLowerCase().contains("dress"), "Clothing category is not displayed.");
+
+        actions.moveToElement(hp.furnitureCategory).click().perform();
+        Assert.assertTrue(hp.chairButton.isDisplayed(), "Furniture category is not displayed.");
+
+        actions.moveToElement(hp.dailyNeedsCategory).click().perform();
+        Assert.assertTrue(hp.nextCategoryTitle.getText().toLowerCase().contains("eat"), "Daily Needs category is not displayed.");
+
+        actions.moveToElement(hp.newCategoryDropDown).click().perform();
+        actions.moveToElement(hp.booksCategory).click().perform();
+        Assert.assertTrue(hp.booksPopular.isDisplayed(), "Books category is not displayed.");
+    }
 }
