405f1665cf3510357498a4314bc006b1aa8d6ece
uguravan <114769074+uguravan@users.noreply.github.com>
2023-01-20 01:18:39 +0300

    bitti

--- src/test/java/Team03/pages/PickBazarUserMenuPage.java
+++ src/test/java/Team03/pages/PickBazarUserMenuPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class PickBazarUserMenuPage {
 
     public PickBazarUserMenuPage(){
@@ -35,14 +37,42 @@ public class PickBazarUserMenuPage {
     @FindBy(xpath = "//button[contains(text(),'Save')]")
     public WebElement profileSaveButton;
 
-    @FindBy(xpath = "(//button[.='Add'])[1]")
-    public WebElement phoneAddButton; //input[@type='tel']
+    @FindBy(xpath = "(//button[contains(@class,'text-sm font-semibold text-accent transition-colors duration')])[1]")
+    public WebElement phoneAddButton;
 
     @FindBy(xpath = "(//input[@type='tel'])[2]")
+    public WebElement addContactBox;
+
+    @FindBy(xpath = "(//button[contains(@class,'ease-in-out focus:outline-none focus:shadow focus:ring')])[2]")
     public WebElement addContactButton;
 
-    @FindBy(xpath = "(//button[.='Add'])[2]")
-    public WebElement adressAddButton;
+    @FindBy(xpath = "(//button[contains(@class,'text-sm font-semibold text-accent transition-colors duration')])[2]")
+    public WebElement newAdressAddButton;
+
+    @FindBy(xpath = "//div[@class='col-span-2']//input[@id='title']")
+    public WebElement newAdressTitleBox;
+
+    @FindBy(xpath = "//div[@class='group relative cursor-pointer rounded border p-4 hover:border-accent border-transparent bg-gray-100']")
+    public List<WebElement> adressBoxes;
+
+    @FindBy(xpath = "//div[contains(@class,'Toastify')]")
+    public WebElement profileUpdatedSuccessfully;
+
+
+    @FindBy(xpath = "//a[.='Change Password']")
+    public WebElement changePasswordSekmesi;
+
+    @FindBy(id = "oldPassword")
+    public WebElement oldPasswordBox;
+
+    @FindBy(id = "newPassword")
+    public WebElement newPasswordBox;
+
+    @FindBy(id = "passwordConfirmation")
+    public WebElement confirmPasswordBox;
+
+    @FindBy(xpath = "//button[.='Submit']")
+    public WebElement passwordSubmitButton;
 
     @FindBy(xpath = "//button[.='My Orders']")
     public WebElement myOrdersButton;
@@ -50,8 +80,17 @@ public class PickBazarUserMenuPage {
     @FindBy(xpath = "//a[.='My Orders']")
     public WebElement myOrdersSekmesi;
 
+    @FindBy(xpath = "(//h3[contains(text(),'My Orders')])[1]")
+    public WebElement myOrdersHeader;
+
+    @FindBy(xpath = "//h2[contains(@class,'flex text-sm font-semibold text-heading md:text-lg')]")
+    public WebElement orderDetailsHeader;
+
     @FindBy(xpath = "//button[.='My Wishlists']")
     public WebElement myWishlistButton;
+
+    @FindBy(xpath = "//button[.='Remove']")
+    public List<WebElement> removeButtonList;
 
     @FindBy(xpath = "//a[.='My Wishlists']")
     public WebElement myWishlistSekmesi;
@@ -68,5 +107,6 @@ public class PickBazarUserMenuPage {
 
     @FindBy(xpath = "//button[.='Login']")
     public WebElement loginButton;
+
 
 }
---
+++ src/test/java/Team03/tests/us07/TestCase07_02.java
@@ -0,0 +1,56 @@
+package Team03.tests.us07;
+
+import Team03.pages.PickBazarHomePage;
+import Team03.pages.PickBazarUserMenuPage;
+import Team03.utilities.ConfigReader;
+import Team03.utilities.Driver;
+import Team03.utilities.TestBaseReports;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class TestCase07_02 extends TestBaseReports {
+//1-Kullanıcı URL gider.
+//2-Kullanıcı valid credentials ile join olur
+//3-Kullanıcı profil fotosuna tıklayarak kullanıcı menüsünü görür.
+//4-Kullanıcı profile sekmesine tıklayarak profile sayfasına gider.
+//5-Kullanıcı cjhange password sekmesine tıklar.
+//6-Kullanıcı açılan pencerede şifre değiştirme adımlarını uygular
+//7-"Password changed succesfully" mesaji gorulmelidir
+    @Test
+    public void login() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        Actions actions = new Actions(Driver.getDriver());
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+        PickBazarHomePage hp = new PickBazarHomePage();
+
+        hp.join.click();
+        hp.emailBox.clear();
+        hp.emailBox.sendKeys(ConfigReader.getProperty("email"));
+        hp.passwordBox.clear();
+        hp.passwordBox.sendKeys(ConfigReader.getProperty("password"));
+        hp.loginButton.click();
+
+        PickBazarUserMenuPage ump = new PickBazarUserMenuPage();
+        ump.profileImg.click();
+        ump.profileButton.click();
+        ump.changePasswordSekmesi.click();
+        wait.until(ExpectedConditions.visibilityOf(ump.oldPasswordBox));
+        ump.oldPasswordBox.sendKeys("Projeteam03-2022");
+        ump.newPasswordBox.sendKeys("Projeteam03-2022");
+        ump.confirmPasswordBox.sendKeys("Projeteam03-2022");
+        ump.passwordSubmitButton.click();
+
+        Thread.sleep(3000);
+        Assert.assertTrue(ump.profileUpdatedSuccessfully.isDisplayed());
+    }
+}
